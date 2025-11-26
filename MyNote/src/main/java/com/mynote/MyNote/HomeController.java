e9a9f6e8061fae35da81ad298167f06f8d99f596
coral <coral.persimmon@gmail.com>
2025-11-27 01:17:35 +0800

    放入gemini模板

--- MyNote/src/main/java/com/mynote/MyNote/HomeController.java
+++ MyNote/src/main/java/com/mynote/MyNote/HomeController.java
@@ -1,17 +1,14 @@
 package com.mynote.MyNote;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-	@GetMapping
-	public String ahowHomePage(Model model)
+	@GetMapping("/")
+	public String ahowHomePage()
 	{
-		model.addAttribute("title","Welcome to our mynote");
-		model.addAttribute("about","I love java");
 		return "home";
 	}
 }
