933a58f904f4727a454b7d583b8fee6341fe6fb1
coral <coral.persimmon@gmail.com>
2025-12-05 02:29:00 +0800

    [mod]將前端拆成:1.登入 2.內頁

--- MyNote/src/main/java/com/mynote/MyNote/HomeController.java
+++ MyNote/src/main/java/com/mynote/MyNote/HomeController.java
@@ -6,9 +6,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping("/")
-	public String ahowHomePage()
-	{
-		return "home";
-	}
+//	@GetMapping("/")
+//	public String ahowHomePage()
+//	{
+//		return "home";
+//	}
+
+	//首頁(login)
+    @GetMapping("/")
+    public String HomePage() {
+        return "login";
+    }
+
+	//login.html
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+
+    //notes.html
+    @GetMapping("/notes")
+    public String showNotesPage() {
+        return "notes";
+    }
 }
